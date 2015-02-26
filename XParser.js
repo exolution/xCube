4435106c0517897723e2599d264814b57d951626
godsong <gaosong@hitour.cc>
2015-02-26 10:27:45 +0800

    update

--- XParser.js
+++ XParser.js
@@ -1,20 +1,14 @@
 /**
- * Created by godsong on 15-1-16.
+ * XParser Node端 MVVM（目前针对avalon.js）模板渲染引擎
+ * 解析指令 ms-href ms-src ms-repeat ms-controller ms-class ms-html ms-text
+ * 未完成指令 ms-if ms-visible
+ * 不支持过滤器 ms-attr （ms-attr-href形式需要转换成ms-href）
+ * @author gaosong
+ * @version 0.5
+ * #TODO 性能需优化 准确性验证 内存泄露检查
  */
 
-
-var _ci = 0;//const increase index
-var ParseState = {
-    TEXT: 0,
-    BEGIN_TAG: 1,
-    TAG_BODY: 2,
-    END_TAG: 3,
-    ATTR_NAME: 4,
-    ATTR_VALUE_BEGIN: 5,
-    ATTR_VALUE: 6,
-    IN_TAG: 7
-};
-var _expando = Math.random().toString().slice(-8), VMName = 'vm_' + _expando;
+var _expando = Math.random().toString().slice(-8), _uuid = 0;
 function boolObject(str) {
     var obj = {};
     var splits = str.split(',');
@@ -24,11 +18,8 @@ function boolObject(str) {
     return obj;
 }
 var _selfClosedTag = boolObject('area,base,basefont,br,col,command,embed,hr,img,input,link,meta,param,source,track,wbr');
-var _needResolveDirective = boolObject('html,text,src,href,repeat');
-var _nonVarsWord = boolObject('Math');
-var re_trim = /^\s*|\s*$/g;
-
-var avalonSniper = /ms-(\w+)(-\w+)*\s*=\s*('|")(.+?)\3/g;
+var re_trim = /^\s*|\s*$/g;//trim正则
+var re_gt = />/g, re_lt = /</g;
 function trim(str) {
     return str.replace(re_trim, '');
 }
@@ -36,6 +27,7 @@ if (typeof window == 'undefined') {
     window = global;
 }
 window.xx = [];
+
 var EvaluatorManager = function () {
     var _evaluatorList = {}, exports = {};
     exports.register = function (name, resolver) {
@@ -46,57 +38,14 @@ var EvaluatorManager = function () {
     };
     return exports;
 }();
-var r_gt = />/g, r_lt = /</g;
-EvaluatorManager.register('text', function (evaluator, result, ctx) {
-    var ret = '';
-    try {
-        ret = evaluator.evaluate(ctx);
-    } catch (e) {
-    }
-    ret = ret.replace(r_gt, '&gt;').replace(r_lt, '&lt;');
-    if (result)result.innerHTML = ret;
-    return ret;
-});
-EvaluatorManager.register('html', function (evaluator, result, ctx) {
-    var ret = '';
-    try {
-        ret = evaluator.evaluate(ctx);
-    } catch (e) {
-    }
-    if (result)result.innerHTML = ret;
-    return ret;
-});
-EvaluatorManager.register('href', function (evaluator, result, ctx) {
-    var ret = '';
-    try {
-        ret = evaluator.evaluate(ctx);
-    } catch (e) {
-    }
-    result.attributes.push({name: 'href', value: ret});
-});
-EvaluatorManager.register('src', function (evaluator, result, ctx) {
-    var ret = '';
-    try {
-        ret = evaluator.evaluate(ctx);
-    } catch (e) {
-    }
-    result.attributes.push({name: 'src', value: ret});
-});
-EvaluatorManager.register('class', function (evaluator, result, ctx) {
-    if (evaluator.evaluate === false) {
-        result.className += evaluator.args + ' ';
-    }
-    else {
-        var ret = false;
-        try {
-            ret = evaluator.evaluate(ctx);
-        } catch (e) {
-        }
-        if (ret) {
-            result.className += evaluator.args + ' ';
-        }
-    }
-});
+function Node(name, body) {
+    this.tagName = name.toLowerCase();
+    this.childNodes = [];
+    this.body = body;
+    this.evaluators = [];
+    resolveAvalonDirective(this, body);
+    this.attributes = [];
+}
 function TextNode(text) {
     this.textContent = text;
 
@@ -125,95 +74,14 @@ TextNode.prototype.compile = function () {
     }
 };
 
-function resolveAvalonDirective(node, body) {
-    var result, code = '';
-    while (result = avalonSniper.exec(body)) {
-        var text = result[4], name = result[1].toLowerCase();
-        if (_needResolveDirective[name]) {
-            var splits = text.split(/{{|}}/);
-            var expr = '';
-            for (var i = 0; i < splits.length; i++) {
-                if (i % 2 == 1) {
-                    code += resolveExpr(splits[i]);
-                    expr += '"+(' + splits[i] + ')+"';
-                }
-                else expr += splits[i];
-            }
-            if (splits.length > 1) {
-                expr = '"' + expr + '"';
-            }
-            else {
-                code = resolveExpr(expr);
-            }
-            var evaluator = {
-                name: name,
-                args: result[2],
-                expr: expr,
-                evaluate: new Function('ctx', code + 'return ' + expr + ';')
-            };
-            if (name == 'repeat') {
-                node.repeat = evaluator;
-            }
-            else {
-                node.evaluators.push(evaluator);
-            }
-
-        }
-        else if (name == 'class') {
-            var tok = text.split(':');
-            if (tok.length > 1) {
-                node.evaluators.push({
-                    name: name,
-                    args: tok[0],
-                    expr: tok[1],
-                    evaluate: new Function('ctx', resolveExpr(tok[1]) + 'return ' + tok[1] + ';')
-                })
-            }
-            else {
-                node.evaluators.push({
-                    name: name,
-                    args: tok[0],
-                    evaluate: false
-                });
-            }
-        }
-        else if (name === 'controller') {
-            node.controller = {
-                expr: trim(text)
-            }
-        }
-
-    }
-
-}
-function resolveEvaluatorResult(result, isSelfClosedTag) {
-    var html = '';
-    for (var i = 0; i < result.attributes.length; i++) {
-        var attribute = result.attributes[i];
-        html += ' ' + attribute.name + '="' + attribute.value + '"';
-    }
-    if (result.className.length > 0) {
-        html += ' class="' + result.className.slice(0, -1) + '"';
-    }
-    if (isSelfClosedTag) {
-        html += '/>';
-        return html;
-    }
-    else {
-        html += '>';
-    }
-
-
-    if (result.innerHTML !== undefined) {
-        html += result.innerHTML;
-    }
-    return html;
-}
-
-function Context(parent, ctrlName) {
+function Context(parent, ctrlName, id) {
     this.parent = parent || null;
     this.ctrlName = ctrlName || (parent && parent.ctrlName);
     this.scope = {};
+    this.id = id;
+    if (this.parent) {
+        this.scope.$outer = this.parent.scope;
+    }
 }
 Context.model = {};
 Context.prototype.getVal = function (name) {
@@ -240,20 +108,21 @@ function render(node, ctx, isRepeat) {
             ctx = new Context(ctx, node.controller.expr);
             delete node.controller;
         }
-        if (isRepeat)console.log(ctx.scope);
-        if (node.repeat) {
+        if (node.repeat && (node.repeat.contextId === undefined || ctx.id != node.repeat.contextId)) {
             var html = '';
             var array;
+            ctx = new Context(ctx, null, _uuid);
+            node.repeat.contextId = _uuid;
+            _uuid++;
             try {
                 array = node.repeat.evaluate(ctx);
             } catch (e) {
             }
             if (array) {
-                var proxy = node.repeat.args.slice(1);
-                delete node.repeat;
+                var proxy = node.repeat.args;
                 if (proxy) {
+                    proxy = proxy.slice(1);
                     for (var idx = 0; idx < array.length; idx++) {
-                        ctx.scope.$outer = {$index: ctx.scope.$index, $outer: ctx.scope.$outer};
                         ctx.scope.$index = idx;
                         ctx.scope.$first = idx == 0;
                         ctx.scope.$last = idx == array.length - 1;
@@ -264,7 +133,6 @@ function render(node, ctx, isRepeat) {
                 else {
                     for (var k in array) {
                         if (array.hasOwnProperty(k)) {
-                            ctx.scope.$outer = {$index: ctx.scope.$index, $outer: ctx.scope.$outer};
                             ctx.scope.$key = k;
                             ctx.scope.$val = array[k];
                             html += render(node, ctx, true);
@@ -278,8 +146,7 @@ function render(node, ctx, isRepeat) {
         for (var i = 0; i < node.evaluators.length; i++) {
             EvaluatorManager.resolve(node.evaluators[i].name, node.evaluators[i], result, ctx);
         }
-        html = node.body;
-        html += resolveEvaluatorResult(result, isSelfClosedTag);
+        html = resolveEvaluatorResult(node.body,result, isSelfClosedTag);
         if (result.innerHTML === undefined) {
             for (i = 0; i < node.childNodes.length; i++) {
                 html += render(node.childNodes[i], ctx, isRepeat);
@@ -292,60 +159,9 @@ function render(node, ctx, isRepeat) {
         return html;
     }
 }
-function _render(node) {
-    if (node.textContent !== undefined) {
-        return node.textContent;
-    }
-    else {
-
-        var html = node.body;
-
-        for (var i = 0; i < node.evaluator.length; i++) {
-
-        }
-        for (i = 0; i < node.childNodes.length; i++) {
-            html += render(node.childNodes[i]);
-        }
-        if (!_selfClosedTag[node.tagName]) {
-            html += '</' + node.tagName + '>';
-        }
-        return html;
-    }
-}
 
 
-function Node(name, body) {
-    this.tagName = name.toLowerCase();
-    this.childNodes = [];
-    this.body = body;
-    this.evaluators = [];
-    resolveAvalonDirective(this, body);
-    this.attributes = [];
-    this.attributesMap = {};
-}
-Node.prototype.addAttribute = function (name, value) {
-    this.attributesMap[name] = value;
-    this.attributes.push(name);
-};
-Node.prototype.toString = function () {
-    if (this.body) {
-        var html = this.body;
-    }
-    else {
-        html = '<' + this.tagName;
-        for (var i = 0; i < this.attributes.length; i++) {
-            html += ' ' + this.attributes[i] + '="' + this.attributesMap[this.attributes[i]] + '"'
-        }
-        html += '>';
-    }
-    for (i = 0; i < this.childNodes.length; i++) {
-        html += this.childNodes[i].toString();
-    }
-    if (!_selfClosedTag[this.tagName]) {
-        html += '</' + this.tagName + '>';
-    }
-    return html;
-};
+
 function _parse(html) {
     var tagSniper = /<([\w:.-]+)(\s+[\w.-]+\s*=\s*('|").*?\3)*\s*\/?>|<\/([\w:.-]+)\s*>/g;
     var result, root = [], curNode, nodeStack = [], lastIndex = 1;
@@ -413,14 +229,109 @@ function _parse(html) {
     }
     return root;
 }
+
 if (typeof exports !== 'undefined') {
     exports.parse = _parse;
     exports.render = render;
     exports.Context = Context;
 }
+
+//region 工具函数区
+var _needResolveDirective = boolObject('html,text,src,href,repeat');
+var avalonSniper = /ms-(\w+)(-\w+)*\s*=\s*('|")(.+?)\3/g;//抓取ms指令正则
+function resolveAvalonDirective(node, body) {
+    var result, code = '';
+    while (result = avalonSniper.exec(body)) {
+        var text = result[4], name = result[1].toLowerCase();
+        if (_needResolveDirective[name]) {
+            var splits = text.split(/{{|}}/);
+            var expr = '';
+            for (var i = 0; i < splits.length; i++) {
+                if (i % 2 == 1) {
+                    code += resolveExpr(splits[i]);
+                    expr += '"+(' + splits[i] + ')+"';
+                }
+                else expr += splits[i];
+            }
+            if (splits.length > 1) {
+                expr = '"' + expr + '"';
+            }
+            else {
+                code = resolveExpr(expr);
+            }
+            var evaluator = {
+                name: name,
+                args: result[2],
+                expr: expr,
+                evaluate: new Function('ctx', code + 'return ' + expr + ';')
+            };
+            if (name == 'repeat') {
+                node.repeat = evaluator;
+            }
+            else {
+                node.evaluators.push(evaluator);
+            }
+
+        }
+        else if (name == 'class') {
+            var tok = text.split(':');
+            if (tok.length > 1) {
+                node.evaluators.push({
+                    name: name,
+                    args: tok[0],
+                    expr: tok[1],
+                    evaluate: new Function('ctx', resolveExpr(tok[1]) + 'return ' + tok[1] + ';')
+                })
+            }
+            else {
+                node.evaluators.push({
+                    name: name,
+                    args: tok[0],
+                    evaluate: false
+                });
+            }
+        }
+        else if (name === 'controller') {
+            node.controller = {
+                expr: trim(text)
+            }
+        }
+
+    }
+
+}
+
+function resolveEvaluatorResult(body,result, isSelfClosedTag) {
+    var html = body;
+    if (result.className.length > 0) {
+        /*html=html.replace(/ class=('|")(.+?)\1/,function(m,a,b){
+            return ' class="'+b+' '+result.className.slice(0, -1);
+        });*/
+        html += ' class="' + result.className.slice(0, -1) + '"';
+    }
+    for (var i = 0; i < result.attributes.length; i++) {
+        var attribute = result.attributes[i];
+        html += ' ' + attribute.name + '="' + attribute.value + '"';
+    }
+
+    if (isSelfClosedTag) {
+        html += '/>';
+        return html;
+    }
+    else {
+        html += '>';
+    }
+
+
+    if (result.innerHTML !== undefined) {
+        html += result.innerHTML;
+    }
+    return html;
+}
 var re_quote = /('|").*?\1/g, re_trans = /\\'|\\"/g, re_operator = /[,+*\/%|&~!()<>:?=-]/, re_idt = /^\s*[a-zA-Z_$]\w*/;
 var _exprCache = {};
-function resolveExpr(expr, vmName) {
+var _nonVarsWord = boolObject('Math');//fixme 需要补充
+function resolveExpr(expr) {
     var code = '';
     expr = trim(expr);
     if (_exprCache[expr]) {
@@ -438,3 +349,78 @@ function resolveExpr(expr, vmName) {
     _exprCache[expr] = code;
     return code;
 }
+//endregion
+
+//region 指令解析器定义区
+EvaluatorManager.register('text', function (evaluator, result, ctx) {
+    var ret = '';
+    try {
+        ret = evaluator.evaluate(ctx);
+    } catch (e) {
+    }
+    ret = ret.replace(re_gt, '&gt;').replace(re_lt, '&lt;');
+    if (result)result.innerHTML = ret;
+    return ret;
+});
+EvaluatorManager.register('html', function (evaluator, result, ctx) {
+    var ret = '';
+    try {
+        ret = evaluator.evaluate(ctx);
+    } catch (e) {
+    }
+    if (result)result.innerHTML = ret;
+    return ret;
+});
+EvaluatorManager.register('href', function (evaluator, result, ctx) {
+    var ret = '';
+    try {
+        ret = evaluator.evaluate(ctx);
+    } catch (e) {
+    }
+    result.attributes.push({name: 'href', value: ret});
+});
+EvaluatorManager.register('src', function (evaluator, result, ctx) {
+    var ret = '';
+    try {
+        ret = evaluator.evaluate(ctx);
+    } catch (e) {
+    }
+    result.attributes.push({name: 'src', value: ret});
+});
+EvaluatorManager.register('class', function (evaluator, result, ctx) {
+    if (evaluator.evaluate === false) {
+        result.className += evaluator.args + ' ';
+    }
+    else {
+        var ret = false;
+        try {
+            ret = evaluator.evaluate(ctx);
+        } catch (e) {
+        }
+        if (ret) {
+            result.className += evaluator.args + ' ';
+        }
+    }
+});
+
+EvaluatorManager.register('visible', function (evaluator, result, ctx) {
+    var ret = false;
+    try {
+        ret = evaluator.evaluate(ctx);
+    } catch (e) {
+    }
+    if (ret) {
+        result.attributes.push({name: 'style', value: 'display:none'});
+    }
+});
+EvaluatorManager.register('if', function (evaluator, result, ctx) {
+    var ret = false;
+    try {
+        ret = evaluator.evaluate(ctx);
+    } catch (e) {
+    }
+    if (ret) {
+        result.attributes.push({name: 'style', value: 'display:none'});
+    }
+});
+//endregion
