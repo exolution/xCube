b58184370248cfeda9749dd09a15f6b9b6b98b9c
godsong <gaosong@hitour.cc>
2015-05-22 12:27:29 +0800

    update

--- Privatization.js
+++ Privatization.js
@@ -23,10 +23,22 @@ UData.prototype = {
 //私有化 db和pri
 UData = Privatization(UData, 'db,pri');
 var udata = new UData();
-console.log(udata.db, udata.pri);//undefined undefined 外部根本访问不到
+
+//外部根本访问不到
+console.log(udata.db, udata.pri);//undefined undefined
 udata.show();//成员变量依然可以访问到
 udata.change();
 udata.show();
+//这种情况是非法的 虽然不会报错 私有变量db是不会被修改的
+
+udata.db=123;
+//虽然此时 udata.db这样访问能访问到123
+console.log(udata.db);// 123
+//但是很明显私有的db并没有被影响
+udata.show();//{ name: 'mongoDB' }
+//但是一旦成员方法使用了该私有变量 外部的udata.db就会被清空
+console.log(udata.db);//undefine
+
 
 /*代码实现区*/
 function factory() {
@@ -43,7 +55,16 @@ function factory() {
             return dest;
         };
         var slice = Array.prototype.slice;
-
+        function splitPrivateData(target,privateDefinition){
+            var privateData={};
+            for(var i=0;i<privateDefinition.length;i++){
+                if(target.hasOwnProperty(privateDefinition[i])){
+                    privateData[privateDefinition[i]]=target[privateDefinition[i]];
+                     delete target[privateDefinition[i]];
+                }
+            }
+            return privateData;
+        }
         function forEach(target, callback) {
             for(var key in target) {
                 if(target.hasOwnProperty(key)) {
@@ -61,22 +82,35 @@ function factory() {
             privates = privates.split(',');
             return function() {
                 var proxyedPrototype = {};//prototype代理
+                var privateData={};
                 forEach(prototype, function(methodName, method) {
                     proxyedPrototype[methodName] = function(Do, Re, Mi, Fa) {//创建每个原型方法的代理函数
+                        merge(instance,privateData);
+                        var ret;
+                        //因为call比apply性能高 所以尽量用call
                         switch(arguments.length) {
                             case 0:
-                                return method.call(realInstance);
+                                ret= method.call(instance);
+                                break;
                             case 1:
-                                return method.call(realInstance, Do);
+                                ret= method.call(instance, Do);
+                                break;
                             case 2:
-                                return method.call(realInstance, Do, Re);
+                                ret= method.call(instance, Do, Re);
+                                break;
                             case 3:
-                                return method.call(realInstance, Do, Re, Mi);
+                                ret= method.call(instance, Do, Re, Mi);
+                                break;
                             case 4:
-                                return method.call(realInstance, Do, Re, Mi, Fa);
+                                ret= method.call(instance, Do, Re, Mi, Fa);
+                                break;
                             default:
-                                return method.apply(realInstance, slice.call(arguments));
+                                ret= method.apply(instance, slice.call(arguments));
+                                break;
                         }
+                        privateData=splitPrivateData(instance,privates);
+                        return this;
+
                     };
                     //代理函数看起来像是真的
                     proxyedPrototype[methodName].toString = function() {
@@ -87,16 +121,14 @@ function factory() {
                 constructor.prototype = proxyedPrototype;
                 //实例化
                 var instance = new constructor();
-                //拷贝一份原始实例
-                var realInstance = merge({}, instance);
+                //取消引用prototype防止泄露
+                constructor.prototype=null;
                 //分离实例中的私有变量
-                for(var i = 0; i < privates.length; i++) {
-                    delete instance[privates[i]];
-                }
+                privateData=splitPrivateData(instance,privates);
                 return instance;
             }
-        }
+        };
     ///////////////////////////////////
     }(module);
-    return module;
+    return module.exports;
 }
